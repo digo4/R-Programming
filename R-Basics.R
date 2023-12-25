ab97ae12ae0859b1b04942c7f4b4fd286431ab6b
digo4 <154576641+digo4@users.noreply.github.com>
2023-12-25 19:20:52 +0530

    Update R-Basics.R

--- R-Basics.R
+++ R-Basics.R
@@ -1,1 +1,16 @@
+### **Getting Started**
+```{r}
+#Here are some examples:
+10+20 #addition
+```
+Assigning variable using `=` or `<-` - assignment operators. 
+```{r}
+a = 20-34 #subtraction
+a
+b = 3*2 #multiplication
+b
+c = 45/5  #division
+c
+42^10 #power 
 
+```
